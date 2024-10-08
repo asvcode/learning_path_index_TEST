b818a268945b255b5c8b5e0035619b29028d2b2e
asvcode <25020209+asvcode@users.noreply.github.com>
2024-10-07 21:53:58 -0700

    Initial commit with local changes

--- app/course-scraper/src/config.py
+++ app/course-scraper/src/config.py
@@ -1,6 +1,7 @@
 class CONFIG:
     # XXX: Modify as needed
-    DATA_PATH = r"C:\Users\user\Documents\coding\learning-path-index\live-demo"
+    #DATA_PATH = r"C:\Users\user\Documents\coding\learning-path-index\live-demo"
+    DATA_PATH = r"C:\PillView\learning-path-index"
 
     # CHROME_USER_DATA_DIR = r"C:\Users\user\AppData\Local\Google\Chrome\User Data"
     # CHROME_USER = "Default"
