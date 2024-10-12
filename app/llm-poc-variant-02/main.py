0815bebf0d237bc827032913227ba266e91caf26
asvcode <25020209+asvcode@users.noreply.github.com>
2024-10-11 23:10:41 -0700

    update

--- app/llm-poc-variant-02/main.py
+++ app/llm-poc-variant-02/main.py
@@ -131,7 +131,7 @@ class GenAILearningPathIndex:
         # The chain_type_kwargs are passed to the chain_type when it is created.
         self.chain_type_kwargs = {"prompt": PROMPT}
         # Create the GenAI project
-        self.llm = OpenAI(temperature=1.0, openai_api_key=self.openai_api_key)
+        self.llm = OpenAI(model="gpt-3.5-turbo-instruct"temperature=1.0, openai_api_key=self.openai_api_key)
     # Get response for query
     # The response is returned as a string.
 
