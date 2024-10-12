10e36737b70f14fe3673372d4c673d207e330ee3
asvcode <25020209+asvcode@users.noreply.github.com>
2024-10-11 21:43:09 -0700

    update

--- app/llm-poc-variant-02/main.py
+++ app/llm-poc-variant-02/main.py
@@ -73,7 +73,7 @@ class GenerateLearningPathIndexEmbeddings:
             print(f' -- WARNING: Found existing FAISS vector store at "{faiss_vectorstore_foldername}", loading from cache.')
             print(f' -- NOTE: Delete the FAISS vector store at "{faiss_vectorstore_foldername}", if you wish to regenerate it from scratch for the next run.')
         self.faiss_vectorstore = FAISS.load_local(
-            "faiss_learning_path_index", self.openai_embeddings
+            "faiss_learning_path_index", self.openai_embeddings, allow_dangerous_deserialization=True
         )
 
     def get_faiss_vector_store(self):
